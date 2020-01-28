c46f4ee732de05bacecaac4d4d990c475901db07
holger420 <hirmo.holger@gmail.com>
2020-01-28 04:55:50 +0200

    lmao 5AM

---
+++ server.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+var supervillains = require('supervillains');
+console.log(supervillains.all);
+var mySupervillainName = supervillains.random();
+console.log(mySupervillainName);
